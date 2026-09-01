3fe422717b1189ed343db04568915a48ff495877
raiilino <raissaoliveiralino@gmail.com>
2026-09-01 14:17:21 -0300

    feat: add Atividade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 import java.util.Scanner;
 
-// Questão 2 -Aprovação do aluno
+// Questão 2 -Aprovação do alunoooo
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
