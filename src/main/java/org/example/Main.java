8f691840c459004579b1f40b98143dfc761a885f
raiilino <raissaoliveiralino@gmail.com>
2026-09-01 14:14:55 -0300

    feat: add Atividade2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite sua idade:");
-        int idade = sc.nextInt();
 
-        if (idade >= 18) {
-            System.out.println("Maior de idade");
+// Questão 2 -Aprovação do aluno
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double nota;
+        System.out.println("Digite sua nota:");
+        nota = entrada.nextDouble();
+
+        if (nota >=7 ){
+            System.out.println("Aprovado!");
         } else {
-            System.out.println("Menor de idade");
+            System.out.println("Reprovado!");
         }
     }
 }
