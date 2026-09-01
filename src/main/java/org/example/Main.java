e1adedad1eb28dc6bea0221dd4db78d68b52410a
raiilino <raissaoliveiralino@gmail.com>
2026-09-01 14:46:06 -0300

    feat: add Atividade4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,23 @@
 package org.example;
 import java.util.Scanner;
 
-// Questão 3 - Maior numero
+// Questão 4 - Classificacao de Desempenho
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int n1, n2;
-        System.out.println("Digite o primeiro número: ");
-        n1 = entrada.nextInt();
-        System.out.println("Digite o segundo número: ");
-        n2 = entrada.nextInt();
+        System.out.println("Digite a sua nota: ");
+        double nota;
+        nota = entrada.nextDouble();
 
-        if (n1 > n2 ){
-            System.out.println(n1);
-        } else {
-            System.out.println(n2);
+
+        if(nota >= 9) {
+            System.out.println("Excelente!");
+        }else if(nota >= 7) {
+            System.out.println("Bom");
+        }else if (nota >= 5){
+            System.out.println("Regular");
+        }else {
+            System.out.println("Pessimo");
         }
     }
 }
