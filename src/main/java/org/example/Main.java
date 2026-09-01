078d460a88bb1fec151f901efe2ae1ff930fd2c6
raiilino <raissaoliveiralino@gmail.com>
2026-09-01 14:30:35 -0300

    feat: add Atividade3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,20 @@
 package org.example;
 import java.util.Scanner;
 
-// Questão 2 -Aprovação do aluno
+// Questão 3 - Maior numero
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double nota;
-        System.out.println("Digite sua nota:");
-        nota = entrada.nextDouble();
+        int n1, n2;
+        System.out.println("Digite o primeiro número: ");
+        n1 = entrada.nextInt();
+        System.out.println("Digite o segundo número: ");
+        n2 = entrada.nextInt();
 
-        if (nota >= 7) {
-            System.out.println("Aprovado!");
+        if (n1 > n2 ){
+            System.out.println(n1);
         } else {
-            System.out.println("Reprovado!");
+            System.out.println(n2);
         }
     }
 }
